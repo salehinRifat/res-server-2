60d4b3e8828bb33e507835285701c8338cec068f
Abu Salehin Rifat <salehinrifat2@gmail.com>
2024-06-10 20:34:10 +0600

    delete operation on cart list of specific user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+// Parse the body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,7 +11,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@salehinrifat1.7tmx0zj.mongodb.net/?retryWrites=true&w=majority&appName=salehinrifat1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,9 +37,22 @@ async function run() {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
         })
+        // Carts operations
         app.post('/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        })
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
         // Connect the client to the server	(optional starting in v4.7)
