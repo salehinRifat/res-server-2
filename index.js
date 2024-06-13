2b6f7d29b8b4cc63315d142013c8f362dd7d3f4b
Abu Salehin Rifat <salehinrifat2@gmail.com>
2024-06-14 02:06:37 +0600

    Added verity token middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 // Parse the body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -28,11 +30,26 @@ async function run() {
         const menuCollection = client.db("RestauDB").collection("menu");
         const reviewsCollection = client.db("RestauDB").collection("reviews");
         const cartCollection = client.db("RestauDB").collection("cart");
-
+        const userCollection = client.db("RestauDB").collection("users");
+        // Token Related API
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETS, { expiresIn: '1h' });
+            res.send({ token });
+        })
+        //Middlewares
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                res.status(401).send({ message: "Forbidden Access" });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+        }
+        // Menu 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
+        // Reviews
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
@@ -53,6 +70,39 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+        // Manage Users
+        app.get('/users', verifyToken, async (req, res) => {
+
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const updatedInfo = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(query, updatedInfo);
+            res.send(result);
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            const query = { email: userInfo.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists' })
+            }
+            const result = await userCollection.insertOne(userInfo);
             res.send(result);
         })
         // Connect the client to the server	(optional starting in v4.7)
